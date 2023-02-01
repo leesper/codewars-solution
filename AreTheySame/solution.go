875e580fc27ba7deb10094eac604246746138cd2
leesper <pascal7718@gmail.com>
2023-02-01 15:38:14 +0800

    done

--- AreTheySame/solution.go
+++ AreTheySame/solution.go
@@ -1,6 +1,30 @@
 package kata
 
+import (
+	"reflect"
+	"sort"
+)
+
+// 输入：两个整数数组array1和array2
+// 输出：布尔值
+// 条件：
+// （1）如果array2中每个元素都是array1中元素的平方，返回true否则false
+// （2）array1或array2可能为空数组
+// （3）array1或array2可能为空指针，此时应返回false
 func Comp(array1 []int, array2 []int) bool {
-	// your code
-	return true
+	if array1 == nil || array2 == nil {
+		return false
+	}
+
+	a1 := array1[:]
+	a2 := array2[:]
+
+	for i, n := range a1 {
+		a1[i] = n * n
+	}
+
+	sort.Ints(a1)
+	sort.Ints(a2)
+
+	return reflect.DeepEqual(a1, a2)
 }
--- AreTheySame/solution_test.go
+++ AreTheySame/solution_test.go
@@ -28,3 +28,30 @@ func TestCase3(t *testing.T) {
 		t.Errorf("Comp(a1, a2) should be false")
 	}
 }
+
+func TestCase4(t *testing.T) {
+	a1 := []int{}
+	a2 := []int{}
+
+	if !Comp(a1, a2) {
+		t.Errorf("Comp(a1, a2) should be true")
+	}
+}
+
+func TestCase5(t *testing.T) {
+	var a1 = []int{121, 144, 19, 161, 19, 144, 19, 11}
+	var a2 = []int{132, 14641, 20736, 361, 25921, 361, 20736, 361}
+
+	if Comp(a1, a2) {
+		t.Errorf("Comp(a1, a2) should be false")
+	}
+}
+
+func TestCase6(t *testing.T) {
+	a1 := []int{121, 144, 19, 161, 19, 144, 19, 11}
+	a2 := []int{121, 14641, 20736, 361, 25921, 361, 20736, 361}
+
+	if !Comp(a1, a2) {
+		t.Errorf("Comp(a1, a2) should be true")
+	}
+}
