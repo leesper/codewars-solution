5818fd5609d7ad72ee05ba2c269c4256b42c8403
leesper <pascal7718@gmail.com>
2023-02-01 18:10:43 +0800

    done

--- PrinterErrors/solution.go
+++ PrinterErrors/solution.go
@@ -1,5 +1,14 @@
 package kata
 
+import "fmt"
+
 func PrinterError(s string) string {
-	return "0/0"
+	denom := len(s)
+	numer := 0
+	for _, r := range s {
+		if r > 'm' {
+			numer++
+		}
+	}
+	return fmt.Sprintf("%d/%d", numer, denom)
 }
